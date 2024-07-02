e1ecb29ffd9fe7331520c84c5c09f8c135fc785a
Angel David <angeldavidstudios@outlook.com>
2024-07-02 16:47:18 -0500

    Inicio del Testeo de Login

---
+++ Test_Login_PF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_Login_PF;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Test_Login_PF/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_Login_PF;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
